bfb7731a8557e01d4a5ff77fffcca417bd9279de
Lynx017 <111288734+Lynx017@users.noreply.github.com>
2022-12-30 16:24:20 +0800

    Create settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc' 
-global.apikey = ["Zeeoneofc", "Alphabot", "Alpha"]
+global.creator = 'Kyy' 
+global.apikey = ["kyy", "017", "MS"]
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
